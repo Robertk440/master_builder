2135603dfd4d33ed5d989e624f7d14a48162b1f1
oninross <oninross@yahoo.com>
2015-05-29 22:31:07 +0800

    Prevent plugin from returning error if selected element is not present

--- _scripts/modules/rr.tableScrollbar.js
+++ _scripts/modules/rr.tableScrollbar.js
@@ -5,25 +5,17 @@
 var RR = (function (parent, $) {
     'use strict';
 
-    $.fn.hasHorizontalScrollBar = function() {
-        return this.get(0).scrollWidth > this.width();
-    };
-
     var wrap = function() {
-        var $contentTable = $('.printableTable');
-
-        if ( !$contentTable.parent().hasClass('table-wrapper') && $contentTable.parent().hasHorizontalScrollBar() ){
-            $contentTable.pdfTable( 'init', {
-                position: 'float', // top, bottom, float
-                orientation: 'l',   // landscape (l), portrait (p)
-                unit: 'pt',         // pt, mm, cm, in.
-                format: 'a4',       // a3, a4, a5, letter, legal
-                marginTop: 20,
-                marginRight: 20,
-                marginBottom: 20,
-                marginLeft: 20,
-            });
-        }
+        $('.printableTable').pdfTable( 'init', {
+            position: 'float', // top, bottom, float
+            orientation: 'l',   // landscape (l), portrait (p)
+            unit: 'pt',         // pt, mm, cm, in.
+            format: 'a4',       // a3, a4, a5, letter, legal
+            marginTop: 20,
+            marginRight: 20,
+            marginBottom: 20,
+            marginLeft: 20,
+        });
     };
 
     var unwrap = function (){
