b18c3356eb1e60bc018063654318b2549b80fe87
Nino Ross Rodriguez <oninross@gmail.com>
2015-06-07 22:15:24 +0800

    Refined Material Design Buttons and Input Boxes

--- _scripts/modules/rr.materialDesign.js
+++ _scripts/modules/rr.materialDesign.js
@@ -8,6 +8,8 @@ var RR = (function (parent, $){
     'use strict';
 
     var setup = function (){
+
+        // Ripple Effect
         var $rippleEffect = $('button, .cta');
 
         $rippleEffect.on('click', function (e){
@@ -16,6 +18,60 @@ var RR = (function (parent, $){
             if ( !$this.hasClass('disabled') ){
                 ripple(e, $this);
             }
+        });
+
+
+        // Hamburger Menu
+        var $materialMenu = $('.material-menu');
+
+        // TimelineMax the menu-icon animation for easier control on Touch/Mouse Events
+        var tl = new TimelineMax();
+
+        tl.to( $materialMenu.find('.top'), 0.2, { top: 4, ease: Expo.easeInOut });
+        tl.to( $materialMenu.find('.bot'), 0.2, { top: -4, ease: Expo.easeInOut }, '-=0.2');
+
+        tl.to( $materialMenu.find('.mid'), 0.2, { opacity: 0, ease: Expo.easeInOut });
+        tl.to( $materialMenu.find('.top'), 0.2, { rotation: 45, ease: Expo.easeInOut }, '-=0.2');
+        tl.to( $materialMenu.find('.bot'), 0.2, { rotation: -45, ease: Expo.easeInOut }, '-=0.2');
+
+
+        // Stop the Timeline at 0 else the animation will play after initiation
+        tl.pause();
+
+        $materialMenu.on('click', function (){
+            var $this = $(this);
+
+            if ( !$this.hasClass('active') ) {
+                $this.addClass('active');
+
+                tl.play();
+            } else {
+                $this.removeClass('active');
+
+                tl.reverse();
+            }
+        });
+
+
+        // Floating Label Input Box
+        $('.floating-input').each(function (){
+            var $this = $(this);
+
+            $this
+                .wrap('<div class="floating"></div>')
+                .before('<span class="placeholder">' + $this.attr('placeholder') + '</span>')
+                .attr('placeholder', '')
+                .on('focus', function (){
+                    var $this = $(this);
+
+                    $this.parent().addClass('focus');
+                }).on('blur', function (){
+                    var $this = $(this);
+
+                    if ( $this.val() === '' ){
+                        $this.parent().removeClass('focus');
+                    }
+                });
         });
     };
 
--- _templates/material-design/index.php
+++ _templates/material-design/index.php
@@ -53,22 +53,35 @@
 		<h3>Raised Buttons</h3>
 		<div class="wrap">
 			<div class="col-3">
-				<button class="btn default shadow-z1">Button</button>
+				<button class="btn default raised">Button</button>
 			</div>
 
 			<div class="col-3">
-				<button class="btn colored shadow-z1">Colored</button>
+				<button class="btn colored raised">Colored</button>
 			</div>
 
 			<div class="col-3">
-				<button class="btn disabled shadow-z1">Disabled</button>
+				<button class="btn disabled raised">Disabled</button>
 			</div>
 
 			<div class="col-3">
-				<a href="#" class="cta shadow-z1">Link</a>
+				<a href="#" class="cta raised">Link</a>
+			</div>
+		</div>
+
+		<h3>Action Buttons</h3>
+		<div class="wrap">
+			<div class="col-3">
+				<button class="menu action material-menu">
+					<span class="line top"></span>
+					<span class="line mid"></span>
+					<span class="line bot"></span>
+				</button>
 			</div>
 		</div>
 	</section>
+
+	<hr/>
 
 	<section class="container-fluid">
 		<h2>Cards and Shadows</h2>
@@ -77,52 +90,63 @@
 			<div class="col-4">
 				<div class="card shadow-z0">
 					<h3>Shadow 0</h3>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nisl arcu, a finibus erat rutrum id. Phasellus pretium pharetra sem at facilisis. Mauris interdum vestibulum faucibus. Sed non arcu nisl. </p>
 				</div>
 			</div>
 
 			<div class="col-4">
 				<div class="card shadow-z1">
 					<h3>Shadow 1</h3>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nisl arcu, a finibus erat rutrum id. Phasellus pretium pharetra sem at facilisis. Mauris interdum vestibulum faucibus. Sed non arcu nisl. </p>
 				</div>
 			</div>
 
 			<div class="col-4">
 				<div class="card shadow-z2">
 					<h3>Shadow 2</h3>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nisl arcu, a finibus erat rutrum id. Phasellus pretium pharetra sem at facilisis. Mauris interdum vestibulum faucibus. Sed non arcu nisl. </p>
 				</div>
 			</div>
 
 			<div class="col-4">
 				<div class="card shadow-z3">
 					<h3>Shadow 3</h3>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nisl arcu, a finibus erat rutrum id. Phasellus pretium pharetra sem at facilisis. Mauris interdum vestibulum faucibus. Sed non arcu nisl. </p>
 				</div>
 			</div>
 
 			<div class="col-4">
 				<div class="card shadow-z4">
 					<h3>Shadow 4</h3>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nisl arcu, a finibus erat rutrum id. Phasellus pretium pharetra sem at facilisis. Mauris interdum vestibulum faucibus. Sed non arcu nisl. </p>
 				</div>
 			</div>
 
 			<div class="col-4">
 				<div class="card shadow-z5">
 					<h3>Shadow 5</h3>
-				</div>
-			</div>
 
-			<div class="col-4">
-				<div class="card shadow-z6">
-					<h3>Shadow 6</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nisl arcu, a finibus erat rutrum id. Phasellus pretium pharetra sem at facilisis. Mauris interdum vestibulum faucibus. Sed non arcu nisl. </p>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<section class="container-fluid">
+	<hr/>
+
+	<!-- <section class="container-fluid">
 		<h2>Radio Buttons</h2>
 
 		<h2>Toggle Buttons</h2>
 	</section>
+
+	<hr/>
+ 	-->
 
 	<section class="container-fluid">
 		<h2>Input</h2>
@@ -135,17 +159,14 @@
 
 			<div class="col-6">
 				<h3>Floating Placeholder</h3>
-				<div class="floating">
-					<span class="placeholder">Label</span>
-					<input type="text" placeholder="Label" />
-				</div>
+				<input class="floating-input" type="text" placeholder="Label" />
 			</div>
 
 			<div class="col-6">
 				<h3>Hints</h3>
 				<div class="floating hint">
 					<input type="text" placeholder="Label" />
-					<span>*Hint: Lorem ipsum dolor</span>
+					<span class="hint">*Hint: Lorem ipsum dolor</span>
 				</div>
 			</div>
 
@@ -156,7 +177,9 @@
 		</div>
 	</section>
 
-	<section class="container-fluid">
+	<hr/>
+
+	<!-- <section class="container-fluid">
 		<h2>Progress Bars</h2>
 
 		<h3>Basic Progress Bar</h3>
@@ -164,7 +187,7 @@
 		<div class="wrap">
 			<progress value="75"></progress>
 		</div>
-	</section>
+	</section> -->
 </div>
 
 <?php include('footer.php'); ?>
